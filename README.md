b6c254f46869b275457e3d62a6297580647246e5
Richard Frost <richard.frost@headforwards.com>
2018-10-04 21:36:53 +0100

    Added code to export converted slide meta into useful data

---
+++ README.md
@@ -0,0 +1,24 @@
+# PPTX Image Extractor
+
+This is a quick node script to extract images from slides; relies on having title fields which separate the other slides.
+
+## Installation
+
+To get setup, clone this repo and run `npm install`
+
+This will download all the necessary dependencies.
+
+## Pre-req before running the scripts
+
+You'll have wanted to convert a PPTX file into an XML output. I think I found a command on my Mac to do this....but don't quote me on this.
+It outputs the PPTX into a `ppt` folder that includes slides and the images. Move these into the top level of this repo to then get the scripts to run against them.
+
+## Running the scripts
+
+Checkout the `package.json`. There are 4 main scripts:
+
+- npm run rename-images : this will rename images to make sure they are numerically ordered
+- npm run rename-slides : this will rename slides to make sure they are numerically ordered
+- npm run extract-text : this extracts the text from all slides so we know what's in them
+- npm run process-slides : this will process all the slides and output the images from each slide into a relevant folder based on the title slide
+
---
+++ images.js
@@ -0,0 +1,162 @@
+const fs = require("fs");
+const fsExtra = require("fs-extra");
+const path = require("path");
+// In newer Node.js versions where process is already global this isn't necessary.
+const process = require("process");
+const parser = require("xml2json");
+const rimraf = require("rimraf");
+
+const baseSlideDir = "./slides/slides/";
+const baseImageDir = "./slides/images/";
+
+const slideDir = "./slides/formatted-slides/";
+const imagesDir = "./slides/formatted-images/";
+const outputDir = "./output/";
+
+rimraf.sync(outputDir);
+fs.mkdirSync(outputDir);
+
+const convertNumber = (file) => {
+    const number = file.substr(5).split(".")[0];
+    const ext = file.split(".")[1];
+    let formattedNum = number;
+
+    if (number.length === 1) {
+        formattedNum = "000" + number;
+    } else if (number.length === 2) {
+        formattedNum = "00" + number;
+    } else if (number.length === 3) {
+        formattedNum = "0" + number;
+    }
+
+    return formattedNum;
+};
+
+// Loop through all the slide files
+const renameSlides = () => {
+    fs.readdir(baseSlideDir, function(err, files) {
+        files.forEach(function(file, index) {
+
+            const formattedNum = convertNumber(file);
+
+            fs.rename(
+                baseSlideDir + file,
+                slideDir + "slide" + formattedNum + "." + ext,
+                function(err) {
+                    if (err) console.log("ERROR: " + err);
+                }
+            );
+        });
+    });
+
+};
+
+// Loop through all the image files
+const renameImages = () => {
+    fs.readdir(baseImageDir, function(err, files) {
+        files.forEach(function(file, index) {
+
+            const formattedNum = convertNumber(file);
+
+            fs.createReadStream(baseImageDir + file).pipe(
+                fs.createWriteStream(
+                    imagesDir + "image" + formattedNum + "." + ext
+                )
+            );
+        });
+    });
+};
+
+// Loop through all the slide files - this will output all text; some are maybe wrong
+const extractSlideText = () => {
+
+    fs.readdir( slideDir, function( err, files ) {
+
+        files.forEach( function( file, index ) {
+
+            fs.readFile(slideDir + file, 'utf8', function(err, data) {
+
+                const json = JSON.parse(parser.toJson(data));
+
+                try {
+                    const title = json["p:sld"]["p:cSld"]["p:spTree"]["p:sp"][0]["p:txBody"]["a:p"]["a:r"]["a:t"];
+
+                    if (title.indexOf('is') > -1) {
+                        console.warn(file, title);
+                    }
+
+                } catch(e) {
+
+                }
+
+             });
+        } );
+    } );
+};
+
+// Loop through all the slide files - this will output all text; some are maybe wrong
+const collateImagesFromSlides = () => {
+    fs.readdir(slideDir, function(err, files) {
+        let currentSeal = "";
+        let picCounter = 0;
+
+        files.forEach(function(file, index) {
+            fs.readFile(slideDir + file, "utf8", function(err, data) {
+                const json = JSON.parse(parser.toJson(data));
+
+                try {
+                    // Work out if this is a title slide
+
+                    const title = json["p:sld"]["p:cSld"]["p:spTree"]["p:sp"][0]["p:txBody"]["a:p"]["a:r"]["a:t"];
+                    const pics = json["p:sld"]["p:cSld"]["p:spTree"]["p:pic"];
+
+                    // If there are no pics then we'll assume this is a definitive title slide
+                    if (!pics) {
+                        console.warn(file, title);
+                        currentSeal = title.replace(" ", "_");
+                    }
+                    try {
+                        fs.mkdirSync(outputDir + currentSeal);
+                    } catch (e) {}
+                } catch (e) {
+                    if (file === "slide0653.xml") {
+                        console.warn(e);
+                    }
+                    // Is not a title slide, so must show seals
+                    try {
+                        const pics = json["p:sld"]["p:cSld"]["p:spTree"]["p:pic"];
+
+                        const imageFiles = fs.readdirSync(imagesDir);
+
+                        for (let i = 0; i < pics.length; i++) {
+                            fsExtra.copySync(
+                                imagesDir + imageFiles[picCounter],
+                                "./output/" +
+                                    currentSeal +
+                                    "/" +
+                                    imageFiles[picCounter]
+                            );
+                            picCounter++;
+                        }
+                    } catch (e) {
+                        if (file === "slide0653.xml") {
+                            console.warn(e);
+                        }
+
+                        console.warn(
+                            file,
+                            "cant find any photos, not sure what to do"
+                        );
+                    }
+                }
+            });
+        });
+    });
+};
+
+module.exports = {
+    renameSlides,
+    renameImages,
+    extractSlideText,
+    collateImagesFromSlides
+};
